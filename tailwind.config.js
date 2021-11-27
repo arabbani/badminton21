21d079ca3a895087ed0c2e9adc94e4d78161547d
Arif Rabbani <arif.93rabbani@gmail.com>
2021-11-27 20:59:46 +0530

    feat: tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
