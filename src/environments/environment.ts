1627275ecd9ca0b13cad7576aab580da279678ed
Arif Rabbani <arif.93rabbani@gmail.com>
2021-12-14 23:11:46 +0530

    feat: changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD2zeZqa1vBF5j89JYPHUCb-pVYmBl0FKE',
+    authDomain: 'badminton-21-5c747.firebaseapp.com',
+    projectId: 'badminton-21-5c747',
+    storageBucket: 'badminton-21-5c747.appspot.com',
+    messagingSenderId: '286174735119',
+    appId: '1:286174735119:web:f45eec8b150b8f00a3ec7a',
+  },
 };
 
 /*
