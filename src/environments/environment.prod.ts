ba319a1a0fd9c5d568cec66260f5e5e98af3cd8c
Arif Rabbani <arif.93rabbani@gmail.com>
2021-12-14 23:11:46 +0530

    feat: changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD2zeZqa1vBF5j89JYPHUCb-pVYmBl0FKE',
+    authDomain: 'badminton-21-5c747.firebaseapp.com',
+    projectId: 'badminton-21-5c747',
+    storageBucket: 'badminton-21-5c747.appspot.com',
+    messagingSenderId: '286174735119',
+    appId: '1:286174735119:web:f45eec8b150b8f00a3ec7a',
+  },
 };
